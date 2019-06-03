46172be738f914e6ca936f75689dfe3cc8345889
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2019-06-03 17:34:48 +0300

    Added bullet shooting (incomplete)

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+
+    public float ExplosionRadius = 20f;
+    public float ExlposionForce = 600f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            
+        }
+    }
+
+}
