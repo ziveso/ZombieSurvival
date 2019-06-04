0ebf8b5ab7c7522d7410df46adff6d8d46e3ebb7
Thongbor Thitiwat TIX18 <thitiwatthongbor@kamk.fi>
2019-06-04 09:32:21 +0300

    finish zombie movement

--- Assets/_Scripts/ZombieController.cs
+++ Assets/_Scripts/ZombieController.cs
@@ -6,35 +6,28 @@ public class ZombieController : MonoBehaviour
 {
     private int health;
     private float movespeed = 1f;
-    private Vector3 movement;
     public Player player;
-
-    private Vector3 findVectorToPlayer()
-    {
-        Vector3 vector = new Vector3(transform.position.x - player.transform.position.x, 0f, transform.position.z - player.transform.position.z);
-        return vector;
-    }
-
+    
     // Start is called before the first frame update
     void Start()
     {
         health = 100;
-        transform.Rotate(new Vector3(0f, 55, 0f));
-        // Debug.Log(findPlayerPosition()[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /**
-        Vector3 targetDir = player.transform.position - transform.position;
-        float angle = Vector3.Angle(targetDir, transform.forward);
-
-        transform.Rotate(new Vector3(0f, - angle, 0f));
-        transform.Translate(new Vector3(movespeed, 0f, 0f) * Time.deltaTime);
-    */
         float step = movespeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        
+        Vector3 targetDir = (player.transform.position - transform.position);
 
+        Debug.Log(targetDir);
+
+        float rstep = 10f * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, rstep,1);
+
+        transform.rotation = Quaternion.LookRotation(newDir, Vector3.up);
     }
 }
